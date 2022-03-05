160500a0c6aab6e6b4b5e43069f1896656f678c7
Fangzhou Yu <yufangzh@oregonstate.edu>
2022-03-04 19:05:10 -0800

    added camera calibration code and fang's calib. images

--- CameraCalibration/Calibration.py
+++ CameraCalibration/Calibration.py
@@ -1,77 +1,68 @@
-#!/usr/bin/env python3
-# encoding:utf-8
+
 import cv2
-import glob
 import numpy as np
-from CalibrationConfig import *
+import os
+import glob
 
-#开始标定, 按任意键关闭最后的图像显示
-
-# termination criteria
+# Defining the dimensions of checkerboard
+CHECKERBOARD = (9,6)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((calibration_size[1]*calibration_size[0],3), np.float32)
-objp[:,:2] = np.mgrid[0:calibration_size[1],0:calibration_size[0]].T.reshape(-1,2)
+# Creating vector to store vectors of 3D points for each checkerboard image
+objpoints = []
+# Creating vector to store vectors of 2D points for each checkerboard image
+imgpoints = [] 
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
 
-#标定采集的图像存储路径
-images = glob.glob(save_path + '*.jpg')
+# Defining the world coordinates for 3D points
+objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
+objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+prev_img_shape = None
+
+# Extracting path of individual image stored in a given directory
+images = glob.glob('./calibration_images/fang_calibration_images/*.jpg')
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (calibration_size[1],calibration_size[0]),None)
-
-    # If found, add object points, image points (after refining them)
+    # If desired number of corners are found in the image then ret = true
+    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+    
+    """
+    If desired number of corner are detected,
+    we refine the pixel coordinates and display 
+    them on the images of checker board
+    """
     if ret == True:
         objpoints.append(objp)
-
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        # refining pixel coordinates for given 2d points.
+        corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
+        
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (calibration_size[1],calibration_size[0]), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(1)
-    else:
-        print('Not find object points:', fname)
+        img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
+    
+    #cv2.imshow('img',img)
+    #cv2.waitKey(0)              
 
 cv2.destroyAllWindows()
 
-#获取内外参
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-print('mtx', mtx)
-print('dist', dist)
+h,w = img.shape[:2]
 
-#误差
-mean_error = 0
-for i in range(len(objpoints)):
-    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
-    mean_error += error
+"""
+Performing camera calibration by 
+passing the value of known 3D points (objpoints)
+and corresponding pixel coordinates of the 
+detected corners (imgpoints)
+"""
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-print ("total error: ", mean_error/len(objpoints))
-
-#保存参数
-np.savez(calibration_param_path, dist_array = dist, mtx_array = mtx, fmt="%d", delimiter=" ")
-print('save successful')
-
-#读取第10张图片测试矫正
-img = cv2.imread(save_path + '10.jpg')
-h, w = img.shape[:2]
-newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-
-# undistort
-mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
-dst = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
-
-cv2.imshow('calibration', dst)
-cv2.imshow('original', img)
-key = cv2.waitKey(0)
-if key != -1:
-    cv2.destroyAllWindows()
+print("Camera matrix : \n")
+print(mtx)
+print("dist : \n")
+print(dist)
+print("rvecs : \n")
+print(rvecs)
+print("tvecs : \n")
+print(tvecs)
